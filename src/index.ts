adc049303c748ec0867ef0ce9e6abb465aa5ce92
Danilo <daniloskype459@gmail.com>
2022-12-27 19:52:34 -0300

    feat: initial environment

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "dotenv/config"
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello world")
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.info(`Application running on ${PORT}`))
